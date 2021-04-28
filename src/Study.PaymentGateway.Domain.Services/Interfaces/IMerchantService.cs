e38a66a20632768785bf3768308c4da7d2c99de5
Gabriel Scavassa <gabriel.scavassa@farfetch.com>
2021-04-28 21:36:07 +0100

    Ading MerchantService

---
+++ src/Study.PaymentGateway.Domain.Services/Interfaces/IMerchantService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Study.PaymentGateway.Domain.Entities.Merchants;
+using Study.PaymentGateway.Domain.Entities.Payments;
+
+namespace Study.PaymentGateway.Domain.Services.Interfaces
+{
+    public interface IMerchantService
+    {
+        Task<Merchant> Get(Guid id);
+
+        Task<List<Payment>> Payments(Guid merchantId);
+
+        Task<Payment> Payment(Guid merchantId, Guid paymentId);
+    }
+}
---
+++ src/Study.PaymentGateway.Domain.Services/MerchantService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Study.PaymentGateway.Domain.Entities.Merchants;
+using Study.PaymentGateway.Domain.Entities.Payments;
+using Study.PaymentGateway.Domain.Services.Interfaces;
+
+namespace Study.PaymentGateway.Domain.Services
+{
+    public class MerchantService : IMerchantService
+    {
+        public Task<Merchant> Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Payment> Payment(Guid merchantId, Guid paymentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Payment>> Payments(Guid merchantId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
